c2cba4db3df08c31902a421c6b003ffd6dc12db6
thiagotars <thiagotarsitano@gmail.com>
2024-08-27 07:32:36 -0300

    fixes CORS in the backend and Url in utils

--- backend/index.js
+++ backend/index.js
@@ -30,17 +30,22 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(
   rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 400, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-    standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    windowMs: 15 * 60 * 1000,
+    limit: 400,
+    standardHeaders: "draft-7",
+    legacyHeaders: false,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tripzz-travel-planner.vercel.app/",
+    credentials: true,
+  })
+);
 
 app.use(xss());
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tripzz-planner.vercel.app/api",
+  baseURL: "https://tripzz-planner.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
