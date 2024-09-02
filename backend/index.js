ab693dc6d699a8c193c29ab54e5353da54934e38
thiagotars <thiagotarsitano@gmail.com>
2024-09-02 12:00:03 -0300

    adds cors options

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://tripzz-travel-planner.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(xss());
 
